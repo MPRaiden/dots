b509b70f21a3e42731450b0ae6597096446b6f19
mpr <marinpericradan1@gmail.com>
2024-10-09 18:36:44 +0200

    Updated noice config
    - moved cmdline popup to center of screen

--- nvim/.config/nvim/lua/custom/plugins/noice.lua
+++ nvim/.config/nvim/lua/custom/plugins/noice.lua
@@ -1,43 +1,62 @@
 return {
   {
     'folke/noice.nvim',
-    event = { 'BufReadPost', 'BufNewFile' },
-    dependencies = {
-      'MunifTanjim/nui.nvim',
-      {
-        'rcarriga/nvim-notify',
-        name = 'notify',
-        opts = {
-          top_down = false,
-          stages = 'static',
-          -- stylua: ignore
-          max_height = function() return math.floor(vim.o.lines * 0.75) end,
-          -- stylua: ignore
-          max_width = function() return math.floor(vim.o.columns * 0.75) end,
+    event = 'VeryLazy', -- This ensures lazy loading based on the "VeryLazy" event
+    opts = {
+      lsp = {
+        -- Override markdown rendering so that cmp and other plugins use Treesitter
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      -- Enable presets for easier configuration
+      presets = {
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+      views = { -- Add this section to configure the popup views
+        cmdline_popup = {
+          position = {
+            row = '50%', -- Center vertically
+            col = '50%', -- Center horizontally
+          },
+          size = {
+            width = 60, -- Width of the popup
+            height = 'auto', -- Height adjusts automatically
+          },
+        },
+        popupmenu = {
+          relative = 'editor', -- Position relative to the editor
+          position = {
+            row = '50%', -- Center vertically
+            col = '50%', -- Center horizontally
+          },
+          size = {
+            width = 60, -- Width of the popup menu
+            height = 10, -- Fixed height for the popup menu
+          },
+          border = {
+            style = 'rounded', -- Add a rounded border
+            padding = { 0, 1 }, -- Padding: 0 for top/bottom, 1 for left/right
+          },
+          win_options = {
+            winhighlight = { -- Customize highlight groups
+              Normal = 'Normal',
+              FloatBorder = 'DiagnosticInfo',
+            },
+          },
         },
       },
     },
-    -- stylua: ignore
-    init = function() vim.opt.lazyredraw = false end,
-    opts = {
-      messages = { view = 'mini', view_warn = 'mini' },
-      lsp = {
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = false,
-          ['vim.lsp.util.stylize_markdown'] = false,
-          ['cmp.entry.get_documentation'] = false,
-        },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = true,
-        lsp_doc_border = true,
-      },
-      routes = {
-        { filter = { event = 'notify', find = 'No information available' }, opts = { skip = true } },
-      },
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      -- Optional dependency for notification view
+      'rcarriga/nvim-notify', -- comment out to remove nvim system messages from the top/right corner
     },
   },
 }
