13a2afbf875ee48b7560fb047437d2f2919a2a68
mpr <marinpericradan1@gmail.com>
2024-10-11 22:38:11 +0200

    Trying to replace tmux with wezter... again

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,78 +1,83 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 local config = {}
 
 config = {
 	enable_wayland = false, -- wezterm doesnt start without this line, see more at https://github.com/wez/wezterm/issues/3121
 	automatically_reload_config = true,
 	window_close_confirmation = "AlwaysPrompt",
-	default_cursor_style = "BlinkingBlock", -- doesn't work in tmux sessions
-	color_scheme = "Rosé Pine (base16)",
+	default_cursor_style = "BlinkingBlock", -- doesn't work in nvim sessions :/
+	color_scheme = "Tokyo Night",
 	adjust_window_size_when_changing_font_size = false,
 	window_decorations = "RESIZE",
 	check_for_updates = false,
 	font_size = 10,
 	font = wezterm.font("JetBrains Mono Nerd Font", { weight = "DemiBold" }),
-	-- window_background_opacity = 0.9, -- uncomment for transparency
-	-- use_fancy_tab_bar = false,
-	enable_tab_bar = false,
+	enable_tab_bar = true,
+	use_fancy_tab_bar = false, -- no effect if enable_tab_bar is set to false
+	-- macos_window_background_blur = 20 -- for mac
+	-- window_background_opacity = 0.9, -- transparency
+	scrollback_lines = 3000,
+	default_workspace = "main",
 	window_padding = {
 		left = 10,
 		right = 10,
-		top = 15,
+		top = 10,
 		bottom = 0,
 	},
-}
 
--- The below is attemp of replacing tmux with wezterm but for not it's not in the stars, maybe one day
--- Set leader key
--- config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
---
--- config.keys = {
---
--- 	-- Pane splitting
--- 	{
--- 		key = "v",
--- 		mods = "LEADER",
--- 		action = wezterm.action.SplitPane({
--- 			direction = "Right",
--- 		}),
--- 	},
--- 	{
--- 		key = "h",
--- 		mods = "LEADER",
--- 		action = wezterm.action.SplitPane({
--- 			direction = "Down",
--- 		}),
--- 	},
---
--- 	-- Enter copy mode
--- 	{ key = "x", mods = "CTRL", action = wezterm.action.ActivateCopyMode },
---
--- 	-- Pane navigation
--- 	{ key = "h", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
--- 	{ key = "j", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
--- 	{ key = "k", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
--- 	{ key = "l", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Right" }) }, -- Tabs
--- 	{ key = "t", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
--- 	{ key = "q", mods = "LEADER", action = wezterm.action({ CloseCurrentTab = { confirm = false } }) },
---
--- 	-- Leader key followed by "l" navigates to last open tab (if no tab nothing happens)
--- 	{
--- 		key = "l",
--- 		mods = "LEADER",
--- 		action = wezterm.action.ActivateLastTab,
--- 	},
---
--- 	-- Switch to specific tabs with Ctrl + Number (can also be done with CTRL + Tab)
--- 	{ key = "1", mods = "LEADER", action = wezterm.action({ ActivateTab = 0 }) },
--- 	{ key = "2", mods = "LEADER", action = wezterm.action({ ActivateTab = 1 }) },
--- 	{ key = "3", mods = "LEADER", action = wezterm.action({ ActivateTab = 2 }) },
--- 	{ key = "4", mods = "LEADER", action = wezterm.action({ ActivateTab = 3 }) },
--- 	{ key = "5", mods = "LEADER", action = wezterm.action({ ActivateTab = 4 }) },
--- 	{ key = "6", mods = "LEADER", action = wezterm.action({ ActivateTab = 5 }) },
--- 	{ key = "7", mods = "LEADER", action = wezterm.action({ ActivateTab = 6 }) },
--- 	{ key = "8", mods = "LEADER", action = wezterm.action({ ActivateTab = 7 }) },
--- 	{ key = "9", mods = "LEADER", action = wezterm.action({ ActivateTab = 8 }) },
--- }
+	-- NOTE: testing phase, very cool setup which replaces most of tmux but no option for preview between tabs and no option for saving sessions (there are some plugins but I can just use tmux if I want to go in that direction)
+
+	-- Dim inactive panes
+	inactive_pane_hsb = {
+		saturation = 0.24,
+		brightness = 0.5,
+	},
+
+	-- Keys
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 },
+	keys = {
+		-- Send C-a when pressing C-a twice
+		{ key = "a", mods = "LEADER|CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
+		{ key = "c", mods = "LEADER", action = act.ActivateCopyMode },
+		{ key = "phys:Space", mods = "LEADER", action = act.ActivateCommandPalette },
+
+		-- 	-- Pane keybindings
+		{ key = "v", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+		{ key = "h", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "h", mods = "CTRL", action = act.ActivatePaneDirection("Left") },
+		{ key = "j", mods = "CTRL", action = act.ActivatePaneDirection("Down") },
+		{ key = "k", mods = "CTRL", action = act.ActivatePaneDirection("Up") },
+		{ key = "l", mods = "CTRL", action = act.ActivatePaneDirection("Right") },
+		{ key = "q", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+		{ key = "o", mods = "LEADER", action = act.RotatePanes("Clockwise") },
+
+		-- 	-- Tab keybindings (navigate tabs)
+		{ key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+		{ key = "[", mods = "CTRL", action = act.ActivateTabRelative(-1) },
+		{ key = "]", mods = "CTRL", action = act.ActivateTabRelative(1) },
+		{ key = "n", mods = "LEADER", action = act.ShowTabNavigator },
+		{
+			-- rename tab
+			key = "e",
+			mods = "LEADER",
+			action = act.PromptInputLine({
+				description = wezterm.format({
+					{ Attribute = { Intensity = "Bold" } },
+					{ Foreground = { AnsiColor = "Fuchsia" } },
+					{ Text = "Renaming Tab Title...:" },
+				}),
+				action = wezterm.action_callback(function(window, pane, line)
+					if line then
+						window:active_tab():set_title(line)
+					end
+				end),
+			}),
+		},
+
+		-- Lastly, workspace
+		{ key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+	},
+}
 
 return config
