a9b3ccad03ece2e5029a7f4613bbb043dc4b4de5
mpr <marinpericradan1@gmail.com>
2024-10-15 16:24:15 +0200

    Updated conform nvim plugin config which will hopefully fix the "old syntax warning I have been getting

--- nvim/.config/nvim/lua/kickstart/plugins/conform.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/conform.lua
@@ -1,39 +1,28 @@
 return {
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    lazy = false,
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_fallback = true }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
+  'stevearc/conform.nvim',
+  config = function()
+    require('conform').setup {
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black', 'pylint' },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        javascript = { { 'prettierd', 'prettier' } },
+        python = { 'isort', 'black' },
+        rust = { 'rustfmt', lsp_format = 'fallback' },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
-    },
-  },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = 'fallback',
+      },
+    }
+
+    -- Format on save
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      pattern = '*',
+      callback = function(args)
+        require('conform').format { bufnr = args.buf }
+      end,
+    })
+
+    -- Bind <leader>f to format
+    vim.api.nvim_set_keymap('n', '<leader>f', ':lua require("conform").format()<CR>', { noremap = true, silent = true })
+  end,
 }
--- vim: ts=2 sts=2 sw=2 et
