8ef8204fa55c95afc5d8f705c448f12a18cbf155
mpr <marinpericradan1@gmail.com>
2024-10-26 17:35:02 +0200

    Color shenanigans p43

--- nvim/.config/nvim/lua/kickstart/plugins/colors.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/colors.lua
@@ -1,9 +1,7 @@
 -- This file is pure pain
 --
---
 -- Centralized function to set highlights and colors
 local function ColorMyPencils(color)
-  color = color or 'tokyonight' -- Default to tokyonight
   vim.cmd.colorscheme(color)
 
   -- Define highlight groups to make transparent
@@ -18,9 +16,12 @@ local function ColorMyPencils(color)
     'TelescopeResultsBorder',
     'TelescopePreviewNormal',
     'TelescopePreviewBorder',
+    'TelescopePromptTitle',
     'WhichKeyFloat', -- Which-Key background
+    'WhichKeyNormal',
     'HarpoonWindow', -- Harpoon window background
     'HarpoonBorder', -- Harpoon border
+    'HarpoonCurrentFile',
     'SignColumn', -- Line numbers/sign column
     'NotifyBackground', -- To handle Notify background warning
   }
@@ -28,8 +29,7 @@ local function ColorMyPencils(color)
   -- Apply transparency to all groups
   for _, group in ipairs(highlight_groups) do
     if group == 'NotifyBackground' then
-      -- Set a default background for NotifyBackground to avoid warnings
-      vim.api.nvim_set_hl(0, group, { bg = '#000000' }) -- Example color
+      vim.api.nvim_set_hl(0, group, { bg = '#000000' }) -- NOTE: This if else is needed to avoid NotifyBackground warning
     else
       vim.api.nvim_set_hl(0, group, { bg = 'none' })
     end
@@ -40,7 +40,7 @@ end
 local function setup_theme(theme_name, options)
   require(theme_name).setup(vim.tbl_extend('force', {
     transparent = true,
-    terminal_colors = true,
+    terminal_colors = false,
     styles = {
       sidebars = 'dark',
       floats = 'dark',
@@ -48,26 +48,23 @@ local function setup_theme(theme_name, options)
   }, options or {}))
 end
 
--- Choose your theme here
-local current_theme = 'tokyonight' -- wezterm 3024 - nvim: tokyonight / wezterm: rose-pine - nvim: rose-pine
-
 -- Color schemes
 return {
   {
     'folke/tokyonight.nvim',
     config = function()
       setup_theme('tokyonight', { disable_background = true, style = 'night', styles = { italic = false } })
-      ColorMyPencils(current_theme)
+      ColorMyPencils 'tokyonight'
     end,
   },
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    config = function()
-      setup_theme('rose-pine', { disable_background = true, style = 'main', styles = { italic = false } })
-      ColorMyPencils(current_theme)
-    end,
-  },
+  -- {
+  --   'rose-pine/neovim',
+  --   name = 'rose-pine',
+  --   config = function()
+  --     setup_theme('rose-pine', { disable_background = true, style = 'main', styles = { italic = false } })
+  --     ColorMyPencils 'rose-pine'
+  --   end,
+  -- },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
