01d2a46d5cb728016437955bf25d5d146dbfa42a
mpr <marinpericradan1@gmail.com>
2024-11-05 14:36:40 +0100

    Small cleanup of nvim colors

--- nvim/.config/nvim/lua/custom/plugins/colors.lua
+++ nvim/.config/nvim/lua/custom/plugins/colors.lua
@@ -2,12 +2,19 @@
 function ColorMyPencils(color)
   vim.cmd.colorscheme(color)
 
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+  --vim.api.nvim_set_hl(0, 'NotifyBackground', { bg = '#000000' })
+
   -- Additional highlights for Telescope and other UI components
   vim.api.nvim_set_hl(0, 'TelescopeNormal', { link = 'Normal' })
   vim.api.nvim_set_hl(0, 'TelescopeBorder', { link = 'Normal' })
   vim.api.nvim_set_hl(0, 'WhichKeyNormal', { link = 'Normal' })
   vim.api.nvim_set_hl(0, 'SignColumn', { link = 'Normal' })
   vim.api.nvim_set_hl(0, 'Pmenu', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'TroubleNormal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'CmpItemMenu', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'Whitespace', { fg = '#4c4c4c' })
   vim.api.nvim_set_hl(0, 'LineNr', { bg = '#000000', fg = '#4c4c4c' })
 end
 
