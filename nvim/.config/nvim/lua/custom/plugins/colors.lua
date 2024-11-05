0db7cff4d9e6579e9e052145d563f8d9fd49ef5d
mpr <marinpericradan1@gmail.com>
2024-11-05 17:55:57 +0100

    Still messing around with colors

--- nvim/.config/nvim/lua/custom/plugins/colors.lua
+++ nvim/.config/nvim/lua/custom/plugins/colors.lua
@@ -15,56 +15,22 @@ function ColorMyPencils(color)
   vim.api.nvim_set_hl(0, 'Whitespace', { fg = '#4c4c4c' })
   vim.api.nvim_set_hl(0, 'LineNr', { bg = '#000000', fg = '#4c4c4c' })
 
+  -- Reduce brightness for yellow-toned highlights
   if color == 'gruvbuddy' then
-    -- Load colorbuddy and set colors
-    local colorbuddy = require 'colorbuddy'
-    local Color = colorbuddy.Color
-    local Group = colorbuddy.Group
-    local c = colorbuddy.colors
-    local g = colorbuddy.groups
-    local s = colorbuddy.styles
-
-    -- Define colors
-    Color.new('white', '#f2e5bc')
-    Color.new('red', '#cc6666')
-    Color.new('pink', '#fef601')
-    Color.new('green', '#99cc99')
-    Color.new('yellow', '#f8fe7a')
-    Color.new('blue', '#81a2be')
-    Color.new('aqua', '#8ec07c')
-    Color.new('cyan', '#8abeb7')
-    Color.new('purple', '#8e6fbd')
-    Color.new('violet', '#b294bb')
-    Color.new('orange', '#de935f')
-    Color.new('brown', '#a3685a')
-    Color.new('seagreen', '#698b69')
-    Color.new('turquoise', '#698b69')
-    local background_string = '#000000'
-    Color.new('background', background_string)
-    Color.new('gray0', background_string)
-
-    -- Define groups with colors
-    --Group.new('Normal', c.white, c.gray0)
-    --Group.new('@constant', c.orange, nil, s.none)
-    -- Group.new('@function', c.yellow, nil, s.none)
-    -- Group.new('@function.bracket', g.Normal, g.Normal)
-    -- Group.new('@keyword', c.violet, nil, s.none)
-    -- Group.new('@keyword.faded', g.NonText.fg:light(), nil, s.none)
-    -- Group.new('@property', c.blue)
-    -- Group.new('@variable', c.white, nil)
-    -- Group.new('@variable.builtin', c.purple:light():light(), g.Normal)
-    -- Group.new('@function.call.lua', c.blue:dark(), nil, nil)
+    vim.api.nvim_set_hl(0, 'Function', { fg = '#d8a657' }) -- Slightly muted yellow
   end
 end
 
 return {
+  -- gruvbuddy
   {
     'tjdevries/colorbuddy.nvim',
+
     lazy = false,
     priority = 1000,
     enabled = true,
     config = function()
-      -- Apply the gruvbuddy theme
+      -- Apply theme
       ColorMyPencils 'gruvbuddy'
     end,
   },
