acc8ffa461b0dca791cbbad1946b8b5613cf429f
mpr <marinpericradan1@gmail.com>
2024-11-20 17:55:41 +0100

    Replacing folkedev with folkes lazydev to see if cmp is really faster

--- nvim/.config/nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/plugins/lspconfig.lua
@@ -6,7 +6,6 @@ return {
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
       { 'j-hui/fidget.nvim', opts = {} },
-      { 'folke/neodev.nvim', opts = {} },
     },
 
     config = function()
@@ -18,8 +17,7 @@ return {
           end
 
           map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition') --  To jump back, press <C-t>.
-          --map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
           map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
           map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
           map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
