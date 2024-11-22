81774a99085964889b0898ede46e1c3e256e147e
mpr <marinpericradan1@gmail.com>
2024-11-22 15:55:10 +0100

    Harpoon and oil color fix

--- nvim/.config/nvim/lua/custom/plugins/colors.lua
+++ nvim/.config/nvim/lua/custom/plugins/colors.lua
@@ -1,50 +1,27 @@
 function Color(color)
   vim.cmd.colorscheme(color)
 
-  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' }) -- Inactive windows
-  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-
-  -- Additional highlights for Telescope and other UI components
+  vim.api.nvim_set_hl(0, 'Normal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'SignColumn', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'CmpItemMenu', { link = 'Normal' })
   vim.api.nvim_set_hl(0, 'TelescopeNormal', { link = 'Normal' })
   vim.api.nvim_set_hl(0, 'TelescopeBorder', { link = 'Normal' })
-  vim.api.nvim_set_hl(0, 'WhichKeyNormal', { link = 'Normal' })
-  vim.api.nvim_set_hl(0, 'SignColumn', { link = 'Normal' })
   vim.api.nvim_set_hl(0, 'Pmenu', { link = 'Normal' })
-  vim.api.nvim_set_hl(0, 'TroubleNormal', { link = 'Normal' })
-  vim.api.nvim_set_hl(0, 'CmpItemMenu', { link = 'Normal' })
-  vim.api.nvim_set_hl(0, 'StatusLine', { link = 'Normal' })
   vim.api.nvim_set_hl(0, 'Whitespace', { fg = '#4c4c4c' })
-
-  if color == 'gruvbuddy' then
-    vim.api.nvim_set_hl(0, 'LineNr', { bg = '#000000', fg = '#4c4c4c' })
-    vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = '#161a2e' })
-  end
+  vim.api.nvim_set_hl(0, 'TroubleNormal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'WhichKeyNormal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none', fg = '#4c4c4c' })
+  vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = '#161a2e' })
 end
 
 return {
   {
     'tjdevries/colorbuddy.nvim',
+    enabled = true,
     lazy = false,
     priority = 1000,
-    enabled = true,
     config = function()
       Color 'gruvbuddy'
-    end,
-  },
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    enabled = false,
-    opts = {},
-    config = function()
-      require('tokyonight').setup {
-        style = 'night',
-        transparent = true,
-        terminal_colors = true,
-      }
-      Color 'tokyonight'
     end,
   },
 }
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,15 +5,13 @@ config = {
 	automatically_reload_config = true,
 	window_close_confirmation = "AlwaysPrompt",
 	default_cursor_style = "BlinkingBlock",
-	color_scheme = "3024 (base16)", -- nice options: "3024 (base16)", "rose-pine", "tokyonight_night"
 	adjust_window_size_when_changing_font_size = false,
 	check_for_updates = false,
 	font_size = 11,
 	font = wezterm.font("JetBrains Mono Nerd Font", { weight = "DemiBold" }),
 	enable_tab_bar = false,
-	use_fancy_tab_bar = false,
 	window_background_opacity = 1,
-	scrollback_lines = 3000,
+	scrollback_lines = 10000,
 	default_workspace = "main",
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" }, -- disables ligatures
 	max_fps = 120,
@@ -30,6 +28,13 @@ config = {
 			italic = true,
 			font = wezterm.font("JetBrains Mono Nerd Font", { italic = false }),
 		},
+	},
+	colors = {
+		foreground = "#FFFFFF", -- White text
+		background = "#000000", -- Black background
+		cursor_bg = "#FFFFFF", -- White cursor background
+		cursor_fg = "#000000", -- Black cursor foreground (inverse)
+		cursor_border = "#FFFFFF", -- White cursor border
 	},
 }
 
