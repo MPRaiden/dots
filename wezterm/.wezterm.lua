4b8f0ab5b8d76af05a3d7ac36cf510ea311583d9
mpr <marinpericradan1@gmail.com>
2024-12-17 16:02:48 +0100

    Updated wezterm as a substitute for tmux

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local config = {}
+local act = wezterm.action
 
 config = {
 	color_scheme = "Apathy (base16)",
@@ -10,7 +11,8 @@ config = {
 	check_for_updates = false,
 	font_size = 11,
 	font = wezterm.font("JetBrains Mono Nerd Font", { weight = "DemiBold" }),
-	enable_tab_bar = false,
+	enable_tab_bar = true,
+	use_fancy_tab_bar = false,
 	window_background_opacity = 1,
 	scrollback_lines = 10000,
 	default_workspace = "main",
@@ -35,6 +37,40 @@ config = {
 		cursor_bg = "#FFFFFF", -- White cursor background
 		cursor_fg = "#000000", -- Black cursor foreground (inverse)
 		cursor_border = "#FFFFFF", -- White cursor border
+	},
+
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 },
+
+	keys = {
+		-- Copy mody
+		{ key = "a", mods = "LEADER|CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
+		{ key = "c", mods = "LEADER", action = act.ActivateCopyMode },
+		{ key = "phys:Space", mods = "LEADER", action = act.ActivateCommandPalette },
+
+		-- Tab keybindings
+		{ key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+		{ key = "x", mods = "LEADER", action = act.CloseCurrentTab({ confirm = true }) },
+		{ key = "[", mods = "CTRL", action = act.ActivateTabRelative(-1) },
+		{ key = "]", mods = "CTRL", action = act.ActivateTabRelative(1) },
+		{ key = "s", mods = "LEADER", action = act.ShowTabNavigator },
+
+		-- rename tab
+		{
+			key = "e",
+			mods = "LEADER",
+			action = act.PromptInputLine({
+				description = wezterm.format({
+					{ Attribute = { Intensity = "Bold" } },
+					{ Foreground = { AnsiColor = "Fuchsia" } },
+					{ Text = "Renaming Tab Title...:" },
+				}),
+				action = wezterm.action_callback(function(window, pane, line)
+					if line then
+						window:active_tab():set_title(line)
+					end
+				end),
+			}),
+		},
 	},
 }
 
