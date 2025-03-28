23b496c6e0d66273e4fe218c595172e3738d0c5a
mpr <marinpericradan1@gmail.com>
2025-03-28 16:49:22 +0100

    removed todo plugin, updated ghostty & nvim colors

--- nvim/.config/nvim/lua/custom/plugins/colors.lua
+++ nvim/.config/nvim/lua/custom/plugins/colors.lua
@@ -28,11 +28,21 @@ return {
   },
   {
     'folke/tokyonight.nvim',
-    enabled = true,
+    enabled = false,
     lazy = false,
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+  {
+    'rose-pine/neovim',
+    enabled = true,
+    lazy = false,
+    priority = 1000,
+    name = 'rose-pine',
+    config = function()
+      vim.cmd.colorscheme 'rose-pine'
     end,
   },
 }
