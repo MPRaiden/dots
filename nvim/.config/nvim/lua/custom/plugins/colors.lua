bdc139d013a115b7cf1e1b1a447de63ce3a626cf
mpr <marinpericradan1@gmail.com>
2025-05-15 18:00:31 +0200

    colors update

--- nvim/.config/nvim/lua/custom/plugins/colors.lua
+++ nvim/.config/nvim/lua/custom/plugins/colors.lua
@@ -26,23 +26,4 @@ return {
       Color 'gruvbuddy'
     end,
   },
-  {
-    'folke/tokyonight.nvim',
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'tokyonight-night'
-    end,
-  },
-  {
-    'rose-pine/neovim',
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    name = 'rose-pine',
-    config = function()
-      Color 'rose-pine'
-    end,
-  },
 }
