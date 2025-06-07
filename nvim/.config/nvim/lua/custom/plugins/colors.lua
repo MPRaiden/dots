ef6ca1014ec89b1706ae3ec47c16d8b761b47939
mpr <marinpericradan1@gmail.com>
2025-06-07 21:13:56 +0200

    chore: added rose-pine color scheme

--- nvim/.config/nvim/lua/custom/plugins/colors.lua
+++ nvim/.config/nvim/lua/custom/plugins/colors.lua
@@ -19,4 +19,24 @@ return {
       require('onedark').load()
     end,
   },
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    enabled = true,
+    layy = false,
+    config = function()
+      require('rose-pine').setup {
+        variant = 'auto',
+        styles = {
+          bold = false,
+          italic = false,
+        },
+      }
+      if vim.g.neovide then
+        vim.g.neovide_padding_top = 20
+        vim.g.neovide_hide_mouse_when_typing = true
+      end
+      vim.cmd 'colorscheme rose-pine'
+    end,
+  },
 }
