031e4737ab23be79e89bc1f3fac547b042ecef06
mpr <marinpericradan1@gmail.com>
2025-06-23 16:14:39 +0200

    lsp uniformed with mac version

--- nvim/.config/nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/plugins/lspconfig.lua
@@ -1,5 +1,5 @@
 return {
-  { -- LSP Configuration & Plugins
+  {
     'neovim/nvim-lspconfig',
     dependencies = {
       { 'williamboman/mason.nvim', config = true },
@@ -9,30 +9,29 @@ return {
     },
 
     config = function()
-      local on_attach = function(event)
-        local group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true })
-        vim.api.nvim_create_autocmd('LspAttach', {
-          group = group,
-          callback = function(ev)
-            local map = function(keys, func, desc)
-              vim.keymap.set('n', keys, func, { buffer = ev.buf, desc = 'LSP: ' .. desc })
-            end
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+        callback = function(event)
+          local map = function(keys, func, desc)
+            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          end
 
-            local fzf = require 'fzf-lua'
-            map('gd', fzf.lsp_definitions, '[G]oto [D]efinition')
-            map('gr', fzf.lsp_references, '[G]oto [R]eferences')
-            map('<leader>D', fzf.lsp_type_definitions, 'Type [D]efinition')
-            map('<leader>ds', fzf.lsp_document_symbols, '[D]ocument [S]ymbols')
-            map('<leader>ws', fzf.lsp_workspace_symbols, '[W]orkspace [S]ymbols')
-            map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-            map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-            map('K', vim.lsp.buf.hover, 'Hover Documentation')
-            map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-          end,
-        })
-      end
+          local fzf = require 'fzf-lua'
 
-      -- Use blink.cmp’s built‑in LSP capabilities instead of cmp_nvim_lsp
+          map('gd', fzf.lsp_definitions, '[G]oto [D]efinition')
+          map('gr', fzf.lsp_references, '[G]oto [R]eferences')
+          map('<leader>D', fzf.lsp_typedefs, 'Type [D]efinition')
+          map('<leader>ds', fzf.lsp_document_symbols, '[D]ocument [S]ymbols')
+          map('<leader>ws', fzf.lsp_workspace_symbols, '[W]orkspace [S]ymbols')
+          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+          map('K', vim.lsp.buf.hover, 'Hover Documentation')
+          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        end,
+      })
+
+      -- NOTE: both options work
+      --local capabilities = vim.lsp.protocol.make_client_capabilities()
       local capabilities = require('blink.cmp').get_lsp_capabilities()
 
       local servers = {
@@ -54,17 +53,18 @@ return {
       }
 
       require('mason').setup()
-      require('mason-tool-installer').setup { ensure_installed = vim.tbl_keys(servers) }
+
+      local ensure_installed = vim.tbl_keys(servers or {})
+      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
-        ensure_installed = vim.tbl_keys(servers),
+        ensure_installed = ensure_installed,
         automatic_enable = true,
         handlers = {
           function(server_name)
-            local server_opts = servers[server_name] or {}
-            server_opts.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server_opts.capabilities or {})
-            server_opts.on_attach = on_attach
-            require('lspconfig')[server_name].setup(server_opts)
+            local server = servers[server_name] or {}
+            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+            require('lspconfig')[server_name].setup(server)
           end,
         },
       }
