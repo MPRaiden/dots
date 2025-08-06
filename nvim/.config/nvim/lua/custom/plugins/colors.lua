5df58bd5dbc6873bd72e987c15c70016cce95b6c
mpr <marinpericradan1@gmail.com>
2025-08-06 19:05:00 +0200

    added colorbuddy

--- nvim/.config/nvim/lua/custom/plugins/colors.lua
+++ nvim/.config/nvim/lua/custom/plugins/colors.lua
@@ -1,3 +1,20 @@
+function Color(color)
+  vim.cmd.colorscheme(color)
+  vim.api.nvim_set_hl(0, 'Normal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'SignColumn', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'CmpItemMenu', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'TelescopeNormal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'TelescopeBorder', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'Pmenu', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'TroubleNormal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'WhichKeyNormal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'Whitespace', { fg = '#4c4c4c' })
+  vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none', fg = '#4c4c4c' })
+  vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = '#161a2e' })
+  vim.api.nvim_set_hl(0, 'FloatBorder', { fg = '#1790eb', bg = 'none' })
+end
+
 return {
   {
     'rose-pine/neovim',
@@ -8,27 +25,21 @@ return {
     config = function()
       require('rose-pine').setup {
         variant = 'moon',
-        dark_variant = 'moon', -- required: expects a string
-        dim_inactive_windows = false, -- required: boolean
-        extend_background_behind_borders = false, -- required: boolean
-
+        dark_variant = 'moon',
+        dim_inactive_windows = false,
+        extend_background_behind_borders = false,
         enable = {
           terminal = true,
           legacy_highlights = true,
           migrations = true,
         },
-
-        groups = {}, -- required
-        highlight_groups = {}, -- required
-
+        groups = {},
+        highlight_groups = {},
         styles = {
           bold = true,
           italic = false,
         },
-
-        before_highlight = function() -- required: at least a placeholder function
-          -- no-op
-        end,
+        before_highlight = function() end,
       }
       vim.cmd.colorscheme 'rose-pine'
 
@@ -58,8 +69,7 @@ return {
       if vim.g.neovide then
         vim.g.neovide_padding_top = 15
       end
-
-      vim.cmd.colorscheme 'tokyonight-night' -- night, storm, day, moon
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
   {
@@ -67,23 +77,20 @@ return {
     enabled = false,
     lazy = false,
     priority = 10000,
-    opts = {
-      transparent = true,
-      terminal_colors = true,
-      styles = {
-        comments = { italic = false },
-        keywords = { italic = false },
-        functions = {},
-        variables = {},
-      },
-      sidebars = { 'qf', 'help' },
-    },
     config = function()
       if vim.g.neovide then
         vim.g.neovide_padding_top = 15
       end
-
       vim.cmd.colorscheme 'melange'
+    end,
+  },
+  {
+    'tjdevries/colorbuddy.nvim',
+    enabled = true,
+    lazy = false,
+    priority = 10000,
+    config = function()
+      Color 'gruvbuddy'
     end,
   },
 }
