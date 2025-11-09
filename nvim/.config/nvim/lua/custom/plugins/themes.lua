093302bad23291c1eca775c6bcc99fb738042d2d
mpr <marinpericradan1@gmail.com>
2025-11-09 10:14:44 +0100

    fix: snacks picker theme to match set theme

--- nvim/.config/nvim/lua/custom/plugins/themes.lua
+++ nvim/.config/nvim/lua/custom/plugins/themes.lua
@@ -29,11 +29,6 @@ return {
       flavour = 'macchiato',
       transparent_background = true,
       term_colors = true,
-      -- dim_inactive = {
-      --   enabled = true,
-      --   shade = 'dark',
-      --   percentage = 0.15,
-      -- },
       no_italic = false,
       no_bold = true,
       styles = {
@@ -43,13 +38,48 @@ return {
         notify = true,
         flash = true,
         blink_cmp = { style = 'bordered' },
-        snacks = { enabled = true, indent_scope_color = '' },
+        snacks = { enabled = true, indent_scope_color = 'mauve' },
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
         lsp_trouble = true,
+        gitsigns = true,
+        mason = true,
       },
     },
     config = function(_, opts)
-      require('catppuccin').setup(opts)
-      vim.cmd.colorscheme 'catppuccin'
+      local catppuccin = require 'catppuccin'
+      catppuccin.setup(opts)
+      vim.cmd.colorscheme 'catppuccin-macchiato'
+
+      -- make sure Neovim uses terminal colors (Ghostty Catppuccin)
+      vim.opt.termguicolors = true
+      vim.g.catppuccin_flavour = 'macchiato'
+      vim.g.catppuccin_terminal_colors = true
+
+      -- fully transparent backgrounds to match Ghostty terminal
+      local transparent_groups = {
+        'Normal',
+        'NormalNC',
+        'NormalFloat',
+        'FloatBorder',
+        'Pmenu',
+        'PmenuSel',
+        'PmenuSbar',
+        'PmenuThumb',
+        'StatusLine',
+        'StatusLineNC',
+        'SnacksNormal',
+        'SnacksBorder',
+        'TelescopeNormal',
+        'TelescopeBorder',
+        'WhichKeyFloat',
+        'CmpPmenu',
+      }
+
+      for _, group in ipairs(transparent_groups) do
+        vim.api.nvim_set_hl(0, group, { bg = 'none' })
+      end
     end,
   },
 }
