97856165d18754e12b4b1411983a1a1594ead03d
mpr <marinpericradan1@gmail.com>
2025-12-02 17:25:45 +0100

    snacks cleanup & disable for annoying flash enhanced motions feature

--- nvim/.config/nvim/lua/custom/plugins/flash.lua
+++ nvim/.config/nvim/lua/custom/plugins/flash.lua
@@ -1,10 +1,13 @@
 return {
-  'folke/flash.nvim',
-  event = 'VeryLazy',
-  ---@type Flash.Config
-  opts = {},
-  -- stylua: ignore
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {
+    modes = {
+      char = { enabled = false },
+    },
+  },
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
   },
 }
+
