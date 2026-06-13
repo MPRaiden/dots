8223b0e498b9270525e4c741f1b6323633d33d19
mpr <marinpericradan1@gmail.com>
2026-06-13 15:25:56 +0200

    horizontal cursor for insert mode

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -25,7 +25,7 @@ vim.opt.scrolloff = 8
 -- vim.opt.cursorline = true
 vim.opt.guicursor = {
   'n-v-c:block', -- Normal, visual, command-line: block cursor
-  'i-ci-ve:ver25', -- Insert, command-line insert, visual-exclude: vertical bar cursor with 25% width
+  'i-ci-ve:hor20', -- Insert, command-line insert, visual-exclude: horizontal bar cursor with 20% height
   'r-cr:hor20', -- Replace, command-line replace: horizontal bar cursor with 20% height
   'o:hor50', -- Operator-pending: horizontal bar cursor with 50% height
   'a:blinkwait700-blinkoff400-blinkon250', -- All modes: blinking settings
