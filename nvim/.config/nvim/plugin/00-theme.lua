856ec099e2261c2441e37a354bab8ced2524a22f
mpr <marinpericradan1@gmail.com>
2026-06-16 18:11:48 +0200

    sync

--- nvim/.config/nvim/plugin/00-theme.lua
+++ nvim/.config/nvim/plugin/00-theme.lua
@@ -1,24 +1,9 @@
 vim.pack.add { 'https://github.com/folke/tokyonight.nvim' }
 
 require('tokyonight').setup {
-  -- transparent = true,
   styles = {
     floats = 'transparent',
   },
 }
 
--- vim.cmd 'colorscheme tokyonight-night'
-
-vim.pack.add {
-  {
-    src = 'https://github.com/rose-pine/neovim',
-    name = 'rose-pine',
-  },
-}
-require('rose-pine').setup {
-  styles = {
-    italic = false,
-    transparency = true,
-  },
-}
-vim.cmd 'colorscheme rose-pine'
+vim.cmd 'colorscheme tokyonight-moon'
