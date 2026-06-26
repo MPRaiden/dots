68aaaf7d4c778c690ea4647f9256028c0473d018
mpr <marinpericradan1@gmail.com>
2026-06-26 12:07:15 +0200

    smear customizations

--- nvim/.config/nvim/plugin/smear.lua
+++ nvim/.config/nvim/plugin/smear.lua
@@ -2,4 +2,10 @@ vim.pack.add {
   'https://github.com/sphamba/smear-cursor.nvim',
 }
 
-require('smear_cursor').setup {}
+require('smear_cursor').setup {
+  opts = {
+    stiffness = 0.5,
+    trailing_stiffness = 0.5,
+    matrix_pixel_threshold = 0.5,
+  },
+}
