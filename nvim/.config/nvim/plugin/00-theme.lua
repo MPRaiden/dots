4855e841893b47543cb3b4cc291002d7e153294a
mpr <marinpericradan1@gmail.com>
2026-06-28 09:18:01 +0200

    fix smear

--- nvim/.config/nvim/plugin/00-theme.lua
+++ nvim/.config/nvim/plugin/00-theme.lua
@@ -1,19 +1,8 @@
-vim.pack.add { 'https://github.com/folke/tokyonight.nvim' }
-
-require('tokyonight').setup {
-  transparent = true,
-  styles = {
-    floats = 'transparent',
-  },
-}
-
--- vim.cmd 'colorscheme tokyonight-day'
-
 vim.pack.add { { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' } }
 
 require('catppuccin').setup {
   flavour = 'mocha', -- latte, frappe, macchiato, mocha
-  transparent_background = false, -- disables setting the background color.
+  transparent_background = true, -- disables setting the background color.
   float = {
     transparent = true, -- enable transparent floating windows
     solid = false, -- use solid styling for floating windows, see |winborder|
--- nvim/.config/nvim/plugin/smear.lua
+++ nvim/.config/nvim/plugin/smear.lua
@@ -3,9 +3,11 @@ vim.pack.add {
 }
 
 require('smear_cursor').setup {
-  opts = {
-    stiffness = 0.5,
-    trailing_stiffness = 0.5,
-    matrix_pixel_threshold = 0.5,
-  },
+  stiffness = 0.8, -- 0.6      [0, 1]
+  trailing_stiffness = 0.6, -- 0.45     [0, 1]
+  stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+  trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
+  damping = 0.95, -- 0.85     [0, 1]
+  damping_insert_mode = 0.95, -- 0.9      [0, 1]
+  distance_stop_animating = 0.5, -- 0.1      > 0}
 }
