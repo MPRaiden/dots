ec26b82e64bc915bffba6c6cda454e195f4ed75a
mpr <marinpericradan1@gmail.com>
2026-07-17 16:34:46 +0200

    sync

--- nvim/.config/nvim/plugin/lspconfig.lua
+++ nvim/.config/nvim/plugin/lspconfig.lua
@@ -10,7 +10,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
   callback = function(event)
     local buf = event.buf
-    local pick = require 'fzf-lua'
+    local pick = require('snacks').picker
     local map = function(keys, func, desc)
       vim.keymap.set('n', keys, func, { buffer = buf, desc = 'LSP: ' .. desc })
     end
@@ -20,7 +20,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end, '[G]oto [D]efinition')
 
     map('gr', function()
-      pick.lsp_references { jump1 = false }
+      pick.lsp_references()
     end, '[G]oto [R]eferences')
 
     map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
--- nvim/.config/nvim/plugin/mini.lua
+++ nvim/.config/nvim/plugin/mini.lua
@@ -1,9 +1,3 @@
--- vim.schedule(function()
---   vim.pack.add { 'https://github.com/echasnovski/mini.misc' }
---   require('mini.misc').setup()
---   MiniMisc.setup_termbg_sync()
--- end)
-
 vim.schedule(function()
   vim.pack.add { 'https://github.com/echasnovski/mini.indentscope' }
   require('mini.indentscope').setup {
