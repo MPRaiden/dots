38da5fcf841994c708ce05b2a579e8e36fec96f2
mpr <marinpericradan1@gmail.com>
2026-07-24 18:48:48 +0200

    sync

--- nvim/.config/nvim/plugin/00-theme.lua
+++ nvim/.config/nvim/plugin/00-theme.lua
@@ -1,55 +1,13 @@
-vim.pack.add { 'https://github.com/rebelot/kanagawa.nvim' }
+vim.pack.add { { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' } }
 
--- Default options:
-require('kanagawa').setup {
-  commentStyle = { italic = true },
-  functionStyle = {},
-  keywordStyle = { italic = false },
-  statementStyle = { bold = true },
-  typeStyle = {},
-  transparent = true, -- do not set background color
-  dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-  terminalColors = true, -- define vim.g.terminal_color_{0,17}
-  colors = { -- add/modify theme and palette colors
-    palette = {},
-    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-  },
-  overrides = function(_)
-    local transparent = { bg = 'none' }
-
-    return {
-      -- Generic floating windows and completion menus
-      NormalFloat = transparent,
-      FloatBorder = transparent,
-      Pmenu = transparent,
-      PmenuExtra = transparent,
-      PmenuKind = transparent,
-
-      -- snacks.nvim picker
-      SnacksPickerNormal = transparent,
-      SnacksPickerBorder = transparent,
-      SnacksPickerInput = transparent,
-      SnacksPickerInputBorder = transparent,
-      SnacksPickerList = transparent,
-      SnacksPickerListBorder = transparent,
-      SnacksPickerPreview = transparent,
-      SnacksPickerPreviewBorder = transparent,
-
-      -- blink.cmp
-      BlinkCmpMenu = transparent,
-      BlinkCmpMenuBorder = transparent,
-      BlinkCmpDoc = transparent,
-      BlinkCmpDocBorder = transparent,
-      BlinkCmpSignatureHelp = transparent,
-      BlinkCmpSignatureHelpBorder = transparent,
-    }
-  end,
-  theme = 'auto', -- Load "wave" theme
-  background = { -- map the value of 'background' option to a theme
-    dark = 'wave', -- try "dragon" !
-    light = 'lotus',
+require('catppuccin').setup {
+  flavour = 'macchiato', -- latte, frappe, macchiato, mocha
+  transparent_background = true, -- disables setting the background color.
+  float = {
+    transparent = true, -- enable transparent floating windows
+    solid = true, -- use solid styling for floating windows, see |winborder|
   },
 }
 
 -- setup must be called before loading
-vim.cmd 'colorscheme kanagawa'
+vim.cmd.colorscheme 'catppuccin-nvim'
