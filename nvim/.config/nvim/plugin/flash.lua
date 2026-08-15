cf7e4d2528b51901f7b90f0b4070d35b1b97dbf7
mpr <marinpericradan1@gmail.com>
2026-08-15 08:05:17 +0200

    sync

---
+++ nvim/.config/nvim/plugin/flash.lua
@@ -0,0 +1,7 @@
+vim.pack.add { 'https://github.com/folke/flash.nvim' }
+
+require('flash').setup {}
+
+vim.keymap.set({ 'n', 'x', 'o' }, 's', function()
+  require('flash').jump()
+end, { desc = 'Flash' })
