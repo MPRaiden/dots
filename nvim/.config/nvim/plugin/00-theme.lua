010233756c4f2af40242803242505d75cca3a1d1
marin peric radan <marinpericradan1@gmail.com>
2026-08-22 14:40:36 +0200

    moved to omarchy

--- nvim/.config/nvim/plugin/00-theme.lua
+++ nvim/.config/nvim/plugin/00-theme.lua
@@ -1,11 +1,14 @@
 vim.pack.add {
   { src = 'https://github.com/catppuccin/nvim', name = 'catppuccin' },
-  { src = 'https://github.com/folke/tokyonight.nvim', name = 'tokyonight' },
 }
 
 require('catppuccin').setup {
-  flavour = 'latte',
+  flavour = 'auto',
   transparent_background = true, -- disables setting the background color.
+  background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+    },
   float = {
     transparent = true, -- enable transparent floating windows
     solid = true, -- use solid styling for floating windows, see |winborder|
@@ -21,35 +24,4 @@ require('catppuccin').setup {
   },
 }
 
-require('tokyonight').setup {
-  style = 'night',
-  transparent = true,
-  styles = {
-    comments = { italic = false },
-    keywords = { italic = false },
-    sidebars = 'dark', -- style for sidebars, see below
-    floats = 'normal', -- style for floating windows
-  },
-}
-
-local active_mode
-
-local function update_colorscheme()
-  local appearance = vim.system({ 'gsettings', 'get', 'org.gnome.desktop.interface', 'color-scheme' }, { text = true }):wait()
-  local mode = appearance.code == 0 and appearance.stdout:match 'dark' and 'dark' or 'light'
-
-  if mode == active_mode then
-    return
-  end
-
-  active_mode = mode
-  vim.o.background = mode
-  vim.cmd.colorscheme(mode == 'dark' and 'tokyonight-night' or 'catppuccin-latte')
-end
-
-update_colorscheme()
-
-vim.api.nvim_create_autocmd({ 'FocusGained', 'VimResume' }, {
-  group = vim.api.nvim_create_augroup('system-colorscheme', { clear = true }),
-  callback = update_colorscheme,
-})
+vim.cmd 'colorscheme catppuccin'
